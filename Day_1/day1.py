c708998cbf9837e8ad459ff544356323d06c717b
Dhrumil Bhatia <dhrumilshivam@gmail.com>
2024-12-04 23:09:47 +0530

    Update day1.py

--- Day_1/day1.py
+++ Day_1/day1.py
@@ -1,13 +1,24 @@
 # Day 1: Historian Hysteria
 
-# def main():
+file_path = 'E:\\LEETCODE\\Advent_Of_Code\\1input.txt'
 
-import pandas as pd
 
-# Load the file
-file_path = '1input.txt'
+with open(file_path, 'r') as file:
+    lines = file.readlines()
 
-# Read the file into a dataframe
-df = pd.read_csv(file_path, sep='\s+', header=None)
-print(df)
+left_list = []
+right_list = []
 
+
+for line in lines:
+    left, right = map(int, line.split())
+    left_list.append(left)
+    right_list.append(right)
+
+left_list.sort()
+right_list.sort()
+
+# total distance
+total_dis = sum(abs(l - r) for l, r in zip(left_list, right_list))
+
+print(total_dis)
